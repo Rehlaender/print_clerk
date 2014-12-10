1ddbd39014e09a9a7ed85e44fd77df154f9a2dc2
Torsten Ruger <torsten@villataika.fi>
2014-12-11 00:24:42 +0200

    render in layout and let css hide rest

--- app/controllers/orders_decorator.rb
+++ app/controllers/orders_decorator.rb
@@ -10,26 +10,22 @@ OrdersController.class_eval do
     load_order
     @invoice = true
     @template = "invoice"
-    render :layout => false
   end
 
   def receipt
     load_order
-    render  :layout => false
   end
 
   def slip
     load_order
     @slip = true
     @template = "slip"
-    render "invoice" ,  :layout => false
   end
 
   def reminder
     load_order
     @reminder = true
     @template = "reminder"
-    render "invoice" ,  :layout => false
   end
 
 end
