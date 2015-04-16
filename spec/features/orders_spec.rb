120de965fe28f98baa04d14f1391de7b7f3cbd00
Torsten Ruger <torsten@villataika.fi>
2015-04-16 19:56:24 +0300

    fix product name
    and add tests for the invoice

--- spec/features/orders_spec.rb
+++ spec/features/orders_spec.rb
@@ -4,29 +4,23 @@ describe "Orders" do
   before(:each) do
     sign_in
   end
-  it "lists orders" do
-    visit_path orders_path
-  end
-  it "should render" do
+  it "should render print buttons" , :js => true do
     order = create :order
     visit_path order_path order
+    OfficeClerk.config(:print_styles).split.each do |style|
+      expect(page).to have_link("" , href: "/orders/#{order.id}/#{style}")
+    end
   end
-  it "creates order from basket" do
-    basket = create(:basket_with_item)
-    visit_path edit_basket_path(basket)
-    find(".make_order").click
-    ensure_path order_path(basket.reload.kori)
-  end
-  it "orders a order" do
-    order = create(:order_ordered)
-    visit_path order_path(order)
-    find(".pay_now").click
-  end
-  it "inventories a order" do
-    order = create(:order_paid)
-    visit_path order_path(order)
-    start = order.basket.items.first.product.inventory
-    find(".ship_now").click
-#    expect(order.basket.items.first.product.inventory).to be order.basket.items.first.quantity
+  describe "invoice" do
+    it "shows total" do
+      order = create :order
+      visit_path "#{order_path(order)}/invoice"
+      expect(page).to have_content I18n.t(:order_total)
+    end
+    it "shows product name" do
+      order = create :order
+      visit_path "#{order_path(order)}/invoice"
+      expect(page).to have_content order.basket.items.first.product.full_name
+    end
   end
 end
